752d412a0002653736d3f01429987846bb7b8706
CiaranAT <2200885@uad.ac.uk>
2024-12-19 20:43:17 +0000

    behaviour tree basics added

---
+++ Scripts/BehaviourTree/chase_player_action.gd
@@ -0,0 +1,31 @@
+extends ActionLeaf
+
+
+#func tick(actor: Node, blackboard: Blackboard) -> int:
+	#return SUCCESS
+
+var target_reached = false
+
+func tick(actor, _blackboard):
+	#actor.is_connected("target_reached")
+
+	if target_reached:
+		return SUCCESS
+	
+	if !target_reached:
+		return RUNNING
+
+#func tick(actor, _blackboard):
+	#if not actor.is_connected("target_reached", self, "_target_reached"):
+		#actor.connect("target_reached", self, "_target_reached")
+		#actor.reset()
+	#if self.target_reached:
+		#self.target_reached = false
+		#actor.disconnect("target_reached", self, "_target_reached")
+		#return SUCCESS
+	#actor.set_target_location(actor.target.global_postion)
+	#return RUNNING
+
+func _target_reached():
+	self.target_reached = true
+	print("target reached signal")
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,8 +1,12 @@
 class_name Enemy
 extends CharacterBody2D
 
+signal target_reached
+
 var movement_speed = 210.0
 @export var target: Node2D = null
+@export var target_location: Vector2:
+	get = get_target_location, set = set_target_location
 
 @onready var navigation_agent = $NavigationAgent2D
 
@@ -13,17 +17,23 @@ func _ready():
 func path_finding_setup():
 	await get_tree().physics_frame
 	if target:
-		navigation_agent.target_position = target.global_position
+		#navigation_agent.target_position = target.global_position
+		set_target_location(target.global_position)
 
-#func accquire_target:
-	
+func get_target_location():
+	return target_location
+
+func set_target_location(value):
+	target_location = value
+	if navigation_agent != null:
+		navigation_agent.target_position = target_location
 
 func _physics_process(delta: float):
 	if navigation_agent.is_navigation_finished():
 		return
 	
-	if target:
-		navigation_agent.target_position = target.global_position
+	#if target:
+		#navigation_agent.target_position = target.global_position
 	
 	var current_agent_position = global_position
 	var next_path_position = navigation_agent.get_next_path_position()
@@ -31,3 +41,8 @@ func _physics_process(delta: float):
 	
 	move_and_slide()
 	pass
+
+
+func _on_navigation_agent_2d_target_reached():
+	emit_signal("target_reached")
+	print("target reached")
